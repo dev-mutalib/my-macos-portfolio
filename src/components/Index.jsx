ae6998db152f06f7360d022539c87952a9f516f3
Abdul Mutalib <mutalib92847@gmail.com>
2026-04-05 23:09:32 +0530

    create Index.jsx for all components files import's

---
+++ src/components/Index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Navbar'
+import Welcome from './Welcome'
+
+const Index = () => {
+  return (
+    <>
+    <Navbar />
+    <Welcome />
+    </>
+  )
+}
+
+export default Index
