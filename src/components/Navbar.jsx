8ff4f6fb0b1d8b8b02943246a10c48c08070482e
Abdul Mutalib <mutalib92847@gmail.com>
2026-04-05 22:37:44 +0530

    create macOS style navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from 'react';
+import dayjs from 'dayjs';
+import { navIcons, navLinks } from '@constants';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <>
+      <nav>
+        <div>
+          <img
+            src='/images/logo.svg'
+            alt='apple-logo'
+          />
+          <p className='font-bold'>Abdul's Portfolio</p>
 
-export default Navbar
+          <ul>
+            {navLinks.map(({ id, name }) => (
+              <li key={id}>
+                <p>{name}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div>
+          <ul>
+            {navIcons.map(({ id, img }) => (
+              <li key={id}>
+                <img
+                  src={img}
+                  alt={`icon-${id}`}
+                />
+              </li>
+            ))}
+          </ul>
+
+          <time>{dayjs().format('DD MMMM YYYY   h:mm a')}</time>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
